f23c30eff095e6eef8a066277ccdd04b8556e4f6
Ilnazdev <minnebaevilnaz@gmail.com>
2024-07-29 18:37:27 +0500

    add cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+# Добавить путь к корневой директории проекта в sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from brain_games.cli import welcome_user
 def welcome_message():
     print('Welcome to the Brain Games!')
 
 
 def main():
     welcome_message()
-
+    welcome_user()
 
 if __name__ == '__main__':
     main()
